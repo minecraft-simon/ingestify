ffac5d8b4ade99fbb6bb720a2f7f26795802de6e
minecraft-simon <minecraft_simon@gmx.net>
2023-11-24 14:07:41 +0100

    Add docker configuration

--- ingestify/build.gradle.kts
+++ ingestify/build.gradle.kts
@@ -14,6 +14,7 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation("io.quarkus:quarkus-container-image-docker")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
